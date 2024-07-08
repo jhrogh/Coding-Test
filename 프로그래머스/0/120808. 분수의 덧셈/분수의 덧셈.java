1880450e0512cc89926ee5a8d8d4d0a46b728f7e
jhrogh <142973582+jhrogh@users.noreply.github.com>
2024-07-08 16:48:40 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.02 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int[] answer = new int[2];
+        int[] test = new int[2];
+        int a = denom1;
+        int b = denom2;
+        
+        // 최대공약수 구하기
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        int gcd = a;
+        
+        // 최소공배수 구하기 -> 분모
+        int lcm = (denom1 * denom2) / gcd;
+        test[1] = lcm;
+        
+        // 분자구하기
+        int x1 = lcm / denom1;
+        int x2 = lcm / denom2;
+        
+        int numerator = (numer1 * x1) + (numer2 * x2);
+        test[0] = numerator;
+        
+        // 기약분수
+        while(numerator != 0) {
+            int temp = lcm % numerator;
+            lcm = numerator;
+            numerator = temp;
+        }
+        int gcd2 = lcm;
+        
+        answer[0] = test[0] / gcd2;
+        answer[1] = test[1] / gcd2;
+        
+        return answer;
+    }
+}
