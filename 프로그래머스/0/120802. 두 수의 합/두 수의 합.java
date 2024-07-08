3450b7a3b5dc54173962a4725d4c3a7094211ae2
jhrogh <142973582+jhrogh@users.noreply.github.com>
2024-07-08 14:30:08 +0900

    [level 0] Title: 두 수의 합, Time: 0.02 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/0/120802. 두 수의 합/두 수의 합.java
+++ 프로그래머스/0/120802. 두 수의 합/두 수의 합.java
@@ -1,7 +1,23 @@
-
+// 정답
 class Solution {
     public int solution(int num1, int num2) {
         int answer = num1 + num2;
         return answer;
     }
 }
+
+// import java.util.Scanner;
+// class Solution {
+//     public int solution(int num1, int num2) {
+//         int result = 0;
+//         result = num1 + num2;
+//         return result;
+//     }
+
+//     public void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int num1 = sc.nextInt();
+//         int num2 = sc.nextInt();
+//         System.out.println(solution(num1, num2));
+//     }
+// }
