24ec55d0275acf8e85bfb62a2904648163512da1
jhrogh <142973582+jhrogh@users.noreply.github.com>
2024-07-10 14:52:01 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -16,3 +16,18 @@ function solution(numer1, denom1, numer2, denom2) {
     
     return answer;
 }
+
+
+
+// 다른 풀이
+// function fnGCD(a, b){
+//     return (a%b)? fnGCD(b, a%b) : b;
+// }
+
+// function solution(denum1, num1, denum2, num2) {
+//     let denum = denum1*num2 + denum2*num1;
+//     let num = num1 * num2;
+//     let gcd = fnGCD(denum, num); //최대공약수
+
+//     return [denum/gcd, num/gcd];
+// }
