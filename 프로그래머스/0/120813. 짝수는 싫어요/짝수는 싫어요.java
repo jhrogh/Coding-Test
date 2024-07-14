11d0c30ec35b32b8a67942f70bf2237938c88b30
jhrogh <142973582+jhrogh@users.noreply.github.com>
2024-07-14 21:54:25 +0900

    [level 0] Title: 짝수는 싫어요, Time: 2.85 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -16,5 +16,14 @@ class Solution {
         answer = oddNumbers.stream().mapToInt(Integer::intValue).toArray();
         
         return answer;
+        
+        // 다른 풀이
+        // List<Integer> answer = new ArrayList<>();
+        // for(int i=1; i<=n; i++){
+        //     if(i % 2 == 1){
+        //         answer.add(i);
+        //     }
+        // }
+        // return answer.stream().mapToInt(x -> x).toArray();
     }
 }
