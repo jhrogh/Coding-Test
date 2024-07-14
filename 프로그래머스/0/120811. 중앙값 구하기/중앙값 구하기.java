9120b1f2b2a13de6ec2c7c0f3875e5b5566b26a9
jhrogh <142973582+jhrogh@users.noreply.github.com>
2024-07-14 13:50:51 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.25 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        // 오름차순 정렬 -버블정렬
+        for(int i = 0; i < array.length - 1; i++) {
+            for(int j = i+1; j < array.length; j++) {
+                if(array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+
+        // 중앙값 구하기
+        int i = array.length / 2;
+        answer = array[i];
+        
+        return answer;
+    }
+}
