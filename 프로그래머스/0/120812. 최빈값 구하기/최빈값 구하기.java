75d64d3d8c8014164a905d73d7dd7247e32aa30c
jhrogh <142973582+jhrogh@users.noreply.github.com>
2024-07-14 21:20:00 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.63 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+
+        for(int num : array) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        int maxFrequency = 0;
+        for(Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            int frequency = entry.getValue();
+            
+            if(frequency > maxFrequency) {
+                maxFrequency = frequency;
+                answer = entry.getKey();
+            }
+            else if(frequency == maxFrequency) {
+                answer = -1;
+            }
+        }
+        return answer;
+    }
+}
