b7f847777f24c9ca01b3476793d5630ec71b0b36
jhrogh <142973582+jhrogh@users.noreply.github.com>
2024-07-17 21:49:29 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -10,6 +10,23 @@ function solution(array, commands) {
         result.sort((a, b) => a - b);
         answer.push(result[k - 1]);
     }
+    
+    // 다른 풀이 1
+    // for(let i = 0; i < commands.length; i++){
+    //     let eachCommand = commands[i]
+    //     let slice = array.slice(eachCommand[0] - 1, eachCommand[1])
+    //           answer.push(slice.sort((a, b) => a - b)[eachCommand[2] - 1])
+    // }
 
+    // 다른 풀이2
+    // for(let value of commands){
+    //     answer.push(array.slice(value[0]-1, value[1]).sort((a,b)=>a-b)[value[2]-1]);
+    // }
+    
+    // 다른 풀이3
+    // answer = commands.map(a=>{
+    //     return array.slice(a[0]-1,a[1]).sort((b,c)=>b-c)[a[2]-1];
+    // })
+    
     return answer;
 }
