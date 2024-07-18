88dd3a674769489944eca78f66eb04a00c5eff12
jhrogh <142973582+jhrogh@users.noreply.github.com>
2024-07-18 22:12:12 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -21,3 +21,18 @@ class Solution {
         return lcm;
     }
 }
+
+
+// 다른 풀이
+// class Solution {
+//     public int solution(int n) {
+//         int answer = 1;
+
+//         while(true){
+//             if(6*answer%n==0) break;
+//             answer++;
+//         }
+
+//         return answer;
+//     }
+// }
