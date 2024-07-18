ebd0f8af5c6fb485e7bc7660057268bae01dec1d
jhrogh <142973582+jhrogh@users.noreply.github.com>
2024-07-18 22:10:36 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,19 +1,9 @@
-class Solution {
+class Solution {        
     public int solution(int n) {
         int answer = 0;
         int lcm;
-        
-        if(n > 6) {
-            lcm = LCM(GCD(n, 6), n);
-            answer = lcm / 6;
-        }
-        else if(6 % n == 0) {
-            answer = 1;
-        }
-        else {
-            lcm = LCM(GCD(6, n),n);
-            answer = lcm / 6;
-        }
+        lcm = LCM(GCD(n, 6), n);
+        answer = lcm / 6;        
         return answer;
     }
     public int GCD(int n, int p) {
