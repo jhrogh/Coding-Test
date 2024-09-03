198504a6f1b956345998547c23bf976e33d23b63
jhrogh <142973582+jhrogh@users.noreply.github.com>
2024-09-03 23:43:14 +0900

    [level 2] Title: H-Index, Time: 0.93 ms, Memory: 67.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        
+        int n = citations.length;
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            int h = n - i;  
+            if (citations[i] >= h) {
+                answer = h;
+                break;
+            }
+        }
+        return answer;
+    }
+}
