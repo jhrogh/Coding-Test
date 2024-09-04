d9d59667fd25e310c0df5ead331ba816e05e9554
jhrogh <142973582+jhrogh@users.noreply.github.com>
2024-09-04 23:58:04 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 33.37 ms, Memory: 64.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
 
 ### 성능 요약
 
-메모리: 96.1 MB, 시간: 68.90 ms
+메모리: 64.7 MB, 시간: 33.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 04일 23:55:42
+2024년 09월 04일 23:58:02
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,19 @@
+function solution(participant, completion) {
+    const map = new Map();
+    
+    participant.forEach(name => {
+        map.set(name, (map.get(name) || 0) + 1);
+    });
+    
+    completion.forEach(name => {
+        map.set(name, map.get(name) - 1);
+    });
+    
+    for (let [name, count] of map) {
+        if (count > 0) {
+            return name;
+        }
+    }
+    
+    return '';
+}
