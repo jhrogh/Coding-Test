cbc15432523820b30f459622a98d7252c633d6f5
jhrogh <142973582+jhrogh@users.noreply.github.com>
2024-09-12 16:23:53 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 104 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,33 +1,39 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Scanner s = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        Set<Integer> set = new HashSet<>();
         int[] height = new int[9];
         int sum = 0;
-        int a = 0, b = 0;
-        
+        int remove1 = 0, remove2 = 0;
+
         for(int i = 0; i < 9; i++) {
-            height[i] = s.nextInt();
+            height[i] = Integer.parseInt(bufferedReader.readLine());
             sum += height[i];
         }
-        
-        for(int i = 0; i < 8; i++) {
-            for(int j = i+1; j < 9; j++) {
-                if(sum - (height[i] + height[j]) == 100) {
-                    a = height[i];
-                    b = height[j];
-                    break;
-                }
+
+        sum -= 100;
+        for(int i = 0; i < 9; i++) {
+            int target = sum - height[i];
+            if(set.contains(target)) {
+                remove1 = height[i];
+                remove2 = target;
+                break;
             }
+            set.add(height[i]);
         }
-        
+
         Arrays.sort(height);
-        for(int h : height) {
-            if(h != a && h != b) {
-                System.out.println(h);
+        for (int n : height) {
+            if (n != remove1 && n != remove2) {
+                System.out.println(n);
             }
         }
-        s.close();
     }
 }
