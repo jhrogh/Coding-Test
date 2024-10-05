317b37e62ca311a6ec32e190e834c173c2fa910a
jhrogh <142973582+jhrogh@users.noreply.github.com>
2024-10-05 23:13:26 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT(id), email, FIRST_NAME, LAST_NAME
+FROM developers
+         JOIN skillcodes ON developers.skill_code & SKILLCODES.CODE
+WHERE NAME ="C#" OR NAME ="Python"
+ORDER BY ID
